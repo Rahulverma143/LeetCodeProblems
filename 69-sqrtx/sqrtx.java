cc60725c6e4e139d054f1d2c14aec9abb495135a
Rahul Verma <rahulvearma17@gmail.com>
2025-12-05 16:40:55 +0530

    Time: 1 ms (92.19%) | Memory: 42.3 MB (43.90%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int mySqrt(int x) {
+long start = 0;
+long end = x;
+int ans = 0;  
+while (start <= end) 
+{        
+    long mid = start +( end - start) / 2;
+
+    if (mid*mid <= x) {
+         ans = (int)mid;
+        start = mid + 1;
+       
+    } 
+    else {
+        end = mid - 1;   }     
+}
+return ans;
+    }
+}
