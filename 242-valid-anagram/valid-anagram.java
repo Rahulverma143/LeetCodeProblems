482316fde03f2541916ae836f6f5757037dab599
Rahul Verma <rahulvearma17@gmail.com>
2025-12-10 16:34:01 +0530

    Time: 4 ms (78.77%) | Memory: 46.2 MB (29.61%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,25 +1,25 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-    //     if(s.length() != t.length())  return false; 
-    //    char[] a = s.toCharArray();
-    //     char[] b = t.toCharArray();
-    //     Arrays.sort(a);
-    //     Arrays.sort(b);
-    //     return Arrays.equals(a,b);
+        if(s.length() != t.length())  return false; 
+       char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        return Arrays.equals(a,b);
 
-      if(s.length() != t.length())  return false; 
-        int[] freq = new int[26];
-        for(int i = 0; i<s.length(); i++){
-               freq[s.charAt(i) - 'a']++;
-        }
-        for(int i = 0; i<t.length(); i++){
-               freq[t.charAt(i) - 'a']--;
-        }
-        for (int i = 0; i < freq.length; i++) {
-         if (freq[i] != 0) {
-        return false;
-          }
-    }
-        return true;
+    //   if(s.length() != t.length())  return false; 
+    //     int[] freq = new int[26];
+    //     for(int i = 0; i<s.length(); i++){
+    //            freq[s.charAt(i) - 'a']++;
+    //     }
+    //     for(int i = 0; i<t.length(); i++){
+    //            freq[t.charAt(i) - 'a']--;
+    //     }
+    //     for (int i = 0; i < freq.length; i++) { 
+    //      if (freq[i] != 0) {
+    //     return false;
+    //       }
+    // }
+    //     return true;
     }
 }
