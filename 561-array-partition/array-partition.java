2b34d9e03a6315534e31d621c3e31cd016bfe54b
Rahul Verma <rahulvearma17@gmail.com>
2025-12-29 00:20:36 +0530

    Time: 17 ms (75.21%) | Memory: 49.6 MB (47.31%) - LeetSync

--- 561-array-partition/array-partition.java
+++ 561-array-partition/array-partition.java
@@ -1,8 +1,20 @@
+// class Solution {
+//     public int arrayPairSum(int[] nums) {
+//         Arrays.sort(nums);
+//         int sum = 0;
+//         for(int i=nums.length-2;i>=0;i-=2){
+//             sum += nums[i];
+//         }
+//         return sum;
+//     }
+// }
 class Solution {
     public int arrayPairSum(int[] nums) {
+
         Arrays.sort(nums);
         int sum = 0;
-        for(int i=nums.length-2;i>=0;i-=2){
+
+        for (int i = 0; i < nums.length; i += 2) {
             sum += nums[i];
         }
         return sum;
