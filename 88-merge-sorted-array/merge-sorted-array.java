cf4e26ce5c9f5001ea2e79098fc07a845e48200c
Rahul Verma <rahulvearma17@gmail.com>
2025-12-29 00:38:09 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (70.16%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,23 +1,42 @@
+// class Solution {
+//     public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+//         int[] temp = new int[m + n]; 
+//         int i = 0, j = 0, k = 0;
+//         while (i < m && j < n) {
+//             if (nums1[i] <= nums2[j]) {
+//                 temp[k++] = nums1[i++];
+//             } else {
+//                 temp[k++] = nums2[j++];
+//             }
+//         }
+//         while (i < m) {
+//             temp[k++] = nums1[i++];
+//         }
+//         while (j < n) {
+//             temp[k++] = nums2[j++];
+//         }
+//         for (int x = 0; x < m + n; x++) {
+//             nums1[x] = temp[x];
+//         }
+//     }
+// }
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
 
-        int[] temp = new int[m + n]; 
-        int i = 0, j = 0, k = 0;
-        while (i < m && j < n) {
-            if (nums1[i] <= nums2[j]) {
-                temp[k++] = nums1[i++];
+        int i = m - 1;          
+        int j = n - 1;         
+        int k = m + n - 1;     
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
             } else {
-                temp[k++] = nums2[j++];
+                nums1[k--] = nums2[j--];
             }
         }
-        while (i < m) {
-            temp[k++] = nums1[i++];
-        }
-        while (j < n) {
-            temp[k++] = nums2[j++];
-        }
-        for (int x = 0; x < m + n; x++) {
-            nums1[x] = temp[x];
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
         }
     }
 }
+
