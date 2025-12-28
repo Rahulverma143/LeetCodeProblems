cc0243f98097b1f09276cd514e2893e783967ac6
Rahul Verma <rahulvearma17@gmail.com>
2025-12-29 00:58:47 +0530

    Time: 1 ms (95.95%) | Memory: 42.9 MB (6.16%) - LeetSync

--- 69-sqrtx/sqrtx.java
+++ 69-sqrtx/sqrtx.java
@@ -1,18 +1,34 @@
+// class Solution {
+//     public int mySqrt(int x) {
+// long start = 0;
+// long end = x;
+// int ans = 0;  
+// while (start <= end) 
+// {        
+//     long mid = start +( end - start) / 2;
+
+//     if (mid*mid <= x) {
+//          ans = (int)mid;
+//         start = mid + 1;
+       
+//     } 
+//     else {
+//         end = mid - 1;   }     
+// }
+// return ans;
+//     }
+// }
 class Solution {
     public int mySqrt(int x) {
-long start = 0;
-long end = x;
-int ans = 0;  
-while (start <= end) 
-{        
-    long mid = start +( end - start) / 2;
-    if (mid*mid <= x) {
-         ans = (int)mid;
-        start = mid + 1; 
-    } 
-    else {
-        end = mid - 1;   }     
-}
-return ans;
+
+        if (x == 0) return 0;
+
+        long r = x;
+
+        while (r * r > x) {
+            r = (r + x / r) / 2;
+        }
+
+        return (int) r;
     }
 }
