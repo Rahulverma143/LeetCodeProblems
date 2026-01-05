28f111586e95c7643765a0508058c7a8b8fd23fe
Rahul Verma <rahulvearma17@gmail.com>
2026-01-05 16:48:17 +0530

    Time: 42 ms (6.47%) | Memory: 104.9 MB (74.11%) - LeetSync

---
+++ 4138-minimum-deletion-cost-to-make-all-characters-equal/minimum-deletion-cost-to-make-all-characters-equal.java
@@ -0,0 +1,20 @@
+class Solution {
+    public long minCost(String s, int[] cost) {
+
+        int n = s.length();
+        long min = Long.MAX_VALUE;  
+
+        for (char c = 'a'; c <= 'z'; c++) {
+            long Cost = 0;
+
+            for (int i = 0; i < n; i++) {
+                char ch = s.charAt(i);
+                if (ch != c) {
+                    Cost += cost[i];
+                }
+            }
+            min = Math.min(min, Cost); 
+        }
+        return min;
+    }
+}
