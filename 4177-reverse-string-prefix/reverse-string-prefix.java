71b0767a8bbc02b7b571204ff24d7c621286fcd5
Rahul Verma <rahulvearma17@gmail.com>
2026-01-17 16:43:49 +0530

    Time: 1 ms (99.85%) | Memory: 44.9 MB (40.78%) - LeetSync

--- 4177-reverse-string-prefix/reverse-string-prefix.java
+++ 4177-reverse-string-prefix/reverse-string-prefix.java
@@ -1,32 +1,32 @@
-class Solution {
-    public String reversePrefix(String s, int k) {
-        int n = s.length();
-        String ans = "";
-        String rev ="";
-        for(int i =k-1 ;i>=0 ;i--){
-            char ch = s.charAt(i);
-            rev += ch;
-        }
-        ans = ans + rev;
-        for(int i = k;i<n ;i++){
-            ans = ans + s.charAt(i);
-        }
-        return ans;
-    }
-}
-
 // class Solution {
 //     public String reversePrefix(String s, int k) {
-//         char arr[] = s.toCharArray();
-//         int start = 0;
-//         int end = k-1;
-//         while(start<end){
-//             char temp = arr[start];
-//             arr[start] = arr[end];
-//             arr[end] = temp;
-//             start++;
-//             end--;            
+//         int n = s.length();
+//         String ans = "";
+//         String rev ="";
+//         for(int i =k-1 ;i>=0 ;i--){
+//             char ch = s.charAt(i);
+//             rev += ch;
 //         }
-//         return new String(arr);
+//         ans = ans + rev;
+//         for(int i = k;i<n ;i++){
+//             ans = ans + s.charAt(i);
+//         }
+//         return ans;
 //     }
 // }
+
+class Solution {
+    public String reversePrefix(String s, int k) {
+        char arr[] = s.toCharArray();
+        int start = 0;
+        int end = k-1;
+        while(start<end){
+            char temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;            
+        }
+        return new String(arr);
+    }
+}
