1b6364a2c6a44f6555dfd43b5246b511173ed96c
Rahul Verma <rahulvearma17@gmail.com>
2026-01-23 17:30:54 +0530

    Time: 3 ms (99.25%) | Memory: 84.9 MB (20.76%) - LeetSync

--- 1206-corporate-flight-bookings/corporate-flight-bookings.java
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.java
@@ -1,16 +1,43 @@
+// class Solution {
+//     public int[] corpFlightBookings(int[][] bookings, int n) {
+//          int[] answer = new int[n];
+//           for (int i = 0; i < bookings.length; i++) {
+//            int first = bookings[i][0];
+//            int last = bookings[i][1]; 
+//            int seats = bookings[i][2];
+//           for (int f = first; f <= last; f++) {
+//               answer[f - 1] += seats;
+//     }
+//  }
+//        return answer;
+//     }
+// }
+
+
+// Tumhe n flights diye gaye hain.
+// Har booking me bataya gaya hai ki flight first se last tak har flight me seats seats book hui hain.
+// Tumhe har flight ke liye total booked seats nikalni hain.
+
 class Solution {
     public int[] corpFlightBookings(int[][] bookings, int n) {
 
-         int[] answer = new int[n];
-          for (int i = 0; i < bookings.length; i++) {
-           int first = bookings[i][0];
-           int last = bookings[i][1]; 
-           int seats = bookings[i][2];
-          for (int f = first; f <= last; f++) {
-              answer[f - 1] += seats;
+        int[] answer = new int[n];
 
-    }
-}
-       return answer;
+       
+        for (int i = 0; i < bookings.length; i++) {
+            int first = bookings[i][0];
+            int last = bookings[i][1];
+            int seats = bookings[i][2];
+
+            answer[first - 1] += seats;      
+            if (last < n) {
+                answer[last] -= seats;      
+            }
+        }
+        for (int i = 1; i < n; i++) {
+            answer[i] += answer[i - 1];
+        }
+
+        return answer;
     }
 }
