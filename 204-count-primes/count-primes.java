b97fffd99c5d2c3bef4537804e9d191f16322eea
Rahul Verma <rahulvearma17@gmail.com>
2026-01-25 23:34:15 +0530

    Time: 106 ms (31.52%) | Memory: 51.1 MB (64.44%) - LeetSync

--- 204-count-primes/count-primes.java
+++ 204-count-primes/count-primes.java
@@ -1,6 +1,5 @@
 class Solution {
     public int countPrimes(int n) {
-          
         if (n <= 2) {
             return 0;
         }  
@@ -13,8 +12,7 @@ class Solution {
          for(int j = i*i; j<n; j+=i)
          isPrime[j] = false;
       }
-    }
-    
+    } 
     int count = 0;
     for(int i = 2; i<n; i++){
         if(isPrime[i]){
